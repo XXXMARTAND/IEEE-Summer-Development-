1afb0b22559dc27cfb61a240b25b6adb8789bf14
lorderikstark <47855413+lorderikstark@users.noreply.github.com>
2019-05-21 22:53:31 +0530

    Delete hello_world1.js

--- hello_world1.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080); 
-
-// this is for the 8080 server and var http is created to connect to hold to request//
